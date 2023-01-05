fc952754563950fc04e9ec9a9d3e31aa130a1d18
sha3er97 <sha3er97@gmail.com>
2023-01-05 22:17:36 +0200

    [4.2] code refactoring and modularization

---
+++ app/src/main/java/com/resala/mosharkaty/utility/classes/NormalVolunteer.java
@@ -0,0 +1,9 @@
+package com.resala.mosharkaty.utility.classes;
+
+public class NormalVolunteer {
+    public String Volname;
+    public String phone_text;
+    public int id;
+    public String motabaa;
+    public int months_count;
+}
